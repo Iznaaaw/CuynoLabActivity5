0e251d2a459daff33e46fe2fa8908834ccfbe018
Cuyno <renzikael@gmail.com>
2022-12-17 15:38:29 +0800

    Cuyno

--- src/SimpleCalc/SimpleCalcGUI.java
+++ src/SimpleCalc/SimpleCalcGUI.java
@@ -7,31 +7,30 @@ import java.awt.event.ActionListener;
 public class SimpleCalcGUI extends  JFrame{
     JFrame frame = new JFrame("Simple Calculator");
     private JPanel PanelMain;
-    private JTextField tfAreaInput1;
+    private JTextField tfNumber1;
     private JComboBox cbOperations;
-    private JButton computeResultButton;
-    private JTextField tfAreaInput2;
-    private JTextField tfResult;
+    private JButton compute;
+    private JTextField tfNumber2;
+    private JTextField lblResult;
 
    public void SimpleCal(){
-       cbOperations = new JComboBox<>(new String[] {"+", "-", "*", "/"});
-           computeResultButton.addActionListener(new ActionListener() {
+           compute.addActionListener(new ActionListener() {
                @Override
                public void actionPerformed(ActionEvent e) {
-                   int num1 = Integer.parseInt(tfAreaInput1.getText());
-                   int num2 = Integer.parseInt(tfAreaInput2.getText());
+                   int num1 = Integer.parseInt(tfNumber1.getText());
+                   int num2 = Integer.parseInt(tfNumber2.getText());
                    int res = 0;
                    String operation = (String)cbOperations.getSelectedItem();
-
-                   if(operation.equals("+")){
+                   if(operation.compareTo("+") == 0){
                        res = num1 + num2;
-                   }else if(operation.equals("-")){
+                   }else if(operation.compareTo("-") == 0){
                        res = num1 - num2;
-                   }else if(operation.equals("*")){
+                   }else if(operation.compareTo("*") == 0){
                        res = num1 * num2;
-                   }else if(operation.equals("/")){
+                   }else if(operation.compareTo("/") == 0){
                        res = num1 / num2;
                    }
+                   lblResult.setText(String.valueOf(res));
           }
      });
    }
